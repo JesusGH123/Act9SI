fcd5ccdf8dc9af9fe689d143970cc31ba4bd6308
JesusGH123 <a01423730@itesm.mx>
2022-05-20 01:32:10 -0500

    Added matrices in the program

--- main.py
+++ main.py
@@ -1,34 +1,77 @@
 import pandas as pd
 
-def findCentroid(point):
+def getDistance(x1, x2):
+  return abs(x2-x1)
+  
+def findCentroid(x2):
   distancesToCentroids = []
-  for i in centroidsPos:
-    distancesToCentroids.append(abs(i -point))
+  for x1 in centroidsPos:
+    distancesToCentroids.append(getDistance(x1, x2))
   min_value = min(distancesToCentroids)
 
-  return distancesToCentroids.index(min_value)
+  return distancesToCentroids.index(min_value), min_value
 
+def getMatrixD():
+  matrixD = [[], [], [], []]
+  for aData in data:
+    matrixD[0].append(aData[1])
+  for i in range(1, len(matrixD)):
+    for aData in matrixD[0]:
+      matrixD[i].append(round(getDistance(centroidsPos[i-1], aData), 2))
+  
+  return matrixD
+  
+def getMatrixG():
+  matrixG = [[], [], [], []]
+  for aData in data:
+    matrixG[0].append(aData[1])
+
+  for j in range(0, len(matrixD[0])):
+    minElem = 1000
+    for i in range(1, len(matrixG)):
+      if(matrixD[i][j] < minElem):
+        minElem = matrixD[i][j]
+    for i in range(1, len(matrixG)):
+        if(matrixD[i][j] == minElem):
+          matrixG[i].append(1)
+        else:
+          matrixG[i].append(0)
+  
+  return matrixG
+  
 #Driver code
 completeData = pd.read_excel('peliculas.xlsx') #Read xlsx
 data = []
 
 #Extract the necessary columns and added to an array
-for i in range(0, 20):
+for i in range(0, len(completeData)):
   data.append((completeData["Movie"].iloc[i], completeData["Genre"].iloc[i]))
 
-#Data for k-means
 centroidsPos = [1,2,3]
 
-for i in range(0, 100):
+limit = int(input("Cuantas iteraciones quieres hacer?: "))
+for i in range(0, limit):
   centroids = [[], [], []]
-  matrixD = centroids
-  matrixG = centroids
-  
-  print("The centroids are in: ", centroidsPos)
-  
+  matrixD = getMatrixD()
+  matrixG = getMatrixG()
+
   for i in range(0, len(data)):
-    current = findCentroid(data[i][1])
-    centroids[current].append(data[i])
+    centroid, distToCentroid = findCentroid(data[i][1])
+    centroids[centroid].append(data[i])  #Append data to the corresponding centroid
+  
+  #Print data in each iteration
+  print("The centroids are in: ", centroidsPos)
+  print("MatrixD: ")
+  for i in range(0, len(matrixD)):
+    if(i>0): 
+      print("C",i, sep="", end=" ")
+    print(matrixD[i])
+  print("MatrixG: ")
+  for i in range(0, len(matrixG)):
+    if(i>0):
+      print("C",i, sep="", end=" ")
+    print(matrixG[i])
+  print("\n")
   
   #Reassign the centroids
   for i in range(0, len(centroids)):
@@ -37,7 +80,8 @@ for i in range(0, 100):
       sum += centroids[i][j][1]
     if(len(centroids[i]) > 0):
       centroidsPos[i] = sum/len(centroids[i])  #Average of the centroid
-    #print("Average in centroid ", i , " is: ", average)
 
 for i in range(1, 4):
-    print("Centroid ",i, centroids[i-1])
+  if(i > 0): print("Centroid ", i)
+  for j in range(0, len(centroids[i-1])):
+    print('\t- Movie: "', centroids[i-1][j][0], '" Genre:', centroids[i-1][j][1], sep="")
